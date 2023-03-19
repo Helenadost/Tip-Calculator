c8698168fb8dd4636012e2bc8caf7a12cb3ebfd9
Helenadost <helenadost@seznam.cz>
2023-03-19 13:19:45 +0100

    updated version with split function

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+
     // call function with input event if something is changed
     document.getElementById('tip-calculator').oninput = function () {
-        // convert bill value to a number
+        const tipInput = document.getElementById('tipInput').value;
+        const tipOutput = document.getElementById('tipOutput');
+        const invalidData = document.getElementById('invalidData');
+        const tipAmount = document.getElementById('tipAmount');
+        const billTotalWithTip = document.getElementById('billTotalWithTip');
+        const perPerson = document.getElementById('perPerson');
+        // convert bill and split value to a number
         const bill = Number(document.getElementById('billTotal').value);
-        // check if bill value is number or not
-        if (!isNaN(bill)) {
-            // get tip element value
-            const tip = document.getElementById('tipInput').value;
+        const split = Number(document.getElementById('split').value);
+
+        // check if bill and split values are number or not
+        if (!isNaN(bill) || !isNaN(split)) {
+
             // set element innetHTML to a tip value
-            document.getElementById('tipOutput').innerHTML = `${tip}%`;
+            tipOutput.innerHTML = `${tipInput}%`;
+
             // calculate final bill value 
-            const tipValue = bill * (tip / 100);
+            const tipValue = bill * (tipInput / 100);
             const finalBill = bill + tipValue;
-            // set final values and round the string to 2 decimals
-            document.getElementById('tipAmount').value = tipValue.toFixed(2);
-            document.getElementById('billTotalWithTip').value = finalBill.toFixed(2);
+            const perPersonCalc = finalBill / split;
+
+            // check if perPersonCalc value is valid
+            if (perPersonCalc <= 0 || isNaN(perPersonCalc) || !isFinite(perPersonCalc)) {
+                // display invalid data notification
+                invalidData.style.display = 'block';
+                tipAmount.value = '';
+                billTotalWithTip.value = '';
+                perPerson.value = '';
+            } else {
+                // set final values and round the string to 2 decimals
+                tipAmount.value = tipValue.toFixed(2);
+                billTotalWithTip.value = finalBill.toFixed(2);
+                perPerson.value = perPersonCalc.toFixed(2);
+                invalidData.style.display = 'none';
+            }
+
             // setting for range input background
-            document.getElementById('tipInput').style.background = `linear-gradient(to right, #59daa4 0%, #59daa4 ${tip}%, #e4f9f0 ${tip}%, #e4f9f0 100%)`;
+            document.getElementById('tipInput').style.background = `linear-gradient(to right, #59daa4 0%, #59daa4 ${tipInput}%, #e4f9f0 ${tipInput}%, #e4f9f0 100%)`;
         } else {
-            alert("Please, insert valid data!")
+            invalidData.style.display = 'block';
         }
     };
 });
